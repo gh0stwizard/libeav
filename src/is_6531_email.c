bcc45908242cda6ea5918a079a7fa4c7705e5c66
gh0stwizard <vitaliy.tokarev@gmail.com>
2017-04-23 06:12:19 +0300

    replace is_utf8_domain #4

--- src/is_6531_email.c
+++ src/is_6531_email.c
@@ -6,57 +6,12 @@
 
 
 extern int
-is_6531_email_fqdn (    idn_resconf_t ctx,
-                        idn_action_t actions,
-                        idn_result_t *r,
-                        const char *email,
-                        size_t length)
-{
-    char *ch = NULL;
-    char *brs = NULL;
-    char *bre = NULL;
-    int rc = 0;
-    const char *end = email + length;
-
-
-    if (length == 0)
-        return inverse(EEAV_EMAIL_EMPTY);
-
-    ch = strrchr (email, '@');
-
-    if (ch == NULL)
-        return inverse(EEAV_DOMAIN_EMPTY);
-
-#ifdef _DEBUG
-    printf (">>> local-part: %.*s (%zu)\n", ch - email, email, ch - email);
-    printf (">>> domain: %s\n", ch + 1);
-#endif
-
-    if (ch - email > VALID_LPART_LEN)
-        return inverse(EEAV_LPART_TOO_LONG);
-
-    rc = is_6531_local (email, ch);
-
-    if (rc != EEAV_NO_ERROR)
-        return rc;
-
-    brs = strchr(ch + 1, '[');
-
-    if (brs == NULL)
-       return (is_utf8_inet_domain (ctx, actions, r, ch + 1, end));
-
-    /* seems to be an ip address */
-    check_ip(); /* see private_email.h */
-}
-
-
-/* allow aliases / non-fqdn */
-extern int
 is_6531_email ( idn_resconf_t ctx,
                 idn_action_t actions,
                 idn_result_t *r,
                 const char *email,
-                size_t length)
+                size_t length,
+                bool tld_check)
 {
     char *ch = NULL;
     char *brs = NULL;
@@ -89,7 +44,7 @@ is_6531_email ( idn_resconf_t ctx,
     brs = strchr(ch + 1, '[');
 
     if (brs == NULL)
-       return (is_utf8_domain (ctx, actions, r, ch + 1, end));
+       return (is_utf8_domain (ctx, actions, r, ch + 1, end, tld_check));
 
     /* seems to be an ip address */
     check_ip(); /* see private_email.h */
