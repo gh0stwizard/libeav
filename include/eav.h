0154cf4e7016f98614d90827d52f742cd67c38b5
gh0stwizard <vitaliy.tokarev@gmail.com>
2017-04-28 19:30:38 +0300

    added comments

--- include/eav.h
+++ include/eav.h
@@ -5,18 +5,17 @@
 #include <stdbool.h>
 
 
-typedef int EAV_RFC;
-enum {
+/* high-level API modes */
+typedef enum {
     EAV_RFC_822,
     EAV_RFC_5321,
     EAV_RFC_5322,
-    EAV_RFC_6531,
-};
+    EAV_RFC_6531
+} EAV_RFC;
 
-/* XXX conflicts with auto-gerated?? */
-typedef int EAV_TLD;
-enum {
-    EAV_TLD_INVALID             = 1 << 1,
+/* TLD types */
+typedef enum {
+    EAV_TLD_INVALID             = 1 << 1, /* internal usage only */
     EAV_TLD_NOT_ASSIGNED        = 1 << 2,
     EAV_TLD_COUNTRY_CODE        = 1 << 3,
     EAV_TLD_GENERIC             = 1 << 4,
@@ -24,8 +23,8 @@ enum {
     EAV_TLD_INFRASTRUCTURE      = 1 << 6,
     EAV_TLD_SPONSORED           = 1 << 7,
     EAV_TLD_TEST                = 1 << 8,
-    EAV_TLD_SPECIAL             = 1 << 9,
-};
+    EAV_TLD_SPECIAL             = 1 << 9
+} EAV_TLD;
 
 /* eav.c utf-8 callback */
 typedef int (*eav_utf8_f) ( idn_resconf_t,
@@ -38,22 +37,21 @@ typedef int (*eav_utf8_f) ( idn_resconf_t,
 typedef int (*eav_ascii_f) (const char *, size_t, bool);
 
 typedef struct eav_s {
-    int             rfc;        /* choose rfc */
-    int             allow_tld;  /* flag: allow only these TLDs */
-    bool            utf8;       /* allow utf8 */
-    bool            tld_check;  /* do fqdn & tld checks */
+    EAV_RFC         rfc;            /* mode */
+    int             allow_tld;      /* flags: allow only these TLDs */
+    bool            tld_check;      /* do fqdn & tld checks */
     /* XXX private */
+    bool            utf8;
     int             errcode;
-    const char      *idnmsg;
-    bool            initialized;
+    const char      *idnmsg;        /* idn error message */
+    bool            initialized;    /* true when idn is initialized */
     idn_resconf_t   idn;
     idn_action_t    actions;
     eav_utf8_f      utf8_cb;
     eav_ascii_f     ascii_cb;
 } eav_t;
 
-/* libeav error codes */
-typedef int TLD_ERROR;
+/* low-level API: error codes */
 enum {
     EEAV_NO_ERROR,
     EEAV_INVALID_RFC,
@@ -92,42 +90,100 @@ enum {
 };
 
 
+/* ---[ high-level API functions ] --- */
+
+
+/*
+ * eav_init: initialize eav structure.
+ */
 extern void
 eav_init (eav_t *eav);
 
+/*
+ * eav_free: free internal resources, but does not free eav structure itself.
+ */
 extern void
 eav_free (eav_t *eav);
 
+/*
+ * eav_setup: confirms choosen user options.
+ *
+ * Returns 0 on success. Otherwise returns EEAV_INVALID_RFC.
+ */
 extern int
 eav_setup (eav_t *eav);
 
+/*
+ * eav_is_email: validates the email and its length.
+ *
+ * Returns 1 if the email is valid. Otherwise returns 0.
+ */
 extern int
 eav_is_email (eav_t *eav, const char *email, size_t length);
 
+
+/*
+ * eav_errstr: returns the error message for the last checked email address.
+ */
 extern const char *
 eav_errstr (eav_t *eav);
 
-extern int
-is_6531_local (const char *start, const char *end);
 
-extern int
-is_5322_local (const char *start, const char *end);
+/* ---[ low-level API functions ] --- */
 
-extern int
-is_5321_local (const char *start, const char *end);
 
+/*
+ * is_822_local: check local-part as defined in RFC 822.
+ */
 extern int
 is_822_local (const char *start, const char *end);
 
+/*
+ * is_5321_local: check local-part as defined in RFC 5321.
+ */
+extern int
+is_5321_local (const char *start, const char *end);
+
+/*
+ * is_5322_local: check local-part as defined in RFC 5322.
+ */
+extern int
+is_5322_local (const char *start, const char *end);
+
+/*
+ * is_6531_local: check local-part as defined in RFC 6531.
+ */
+extern int
+is_6531_local (const char *start, const char *end);
+
+/*
+ * is_ipv4: validate IPv4 address.
+ */
 extern int
 is_ipv4 (const char *start, const char *end);
 
+/*
+ * is_ipv6: validate IPv6 address.
+ */
 extern int
 is_ipv6 (const char *start, const char *end);
 
+/*
+ * is_ipaddr: validates if the address IPv4 or IPv6
+ */
+extern int
+is_ipaddr (const char *start, const char *end);
+
+/*
+ * is_ascii_domain: validates if the domain is all-ASCII.
+ */
 extern int
 is_ascii_domain (const char *start, const char *end);
 
+
+/*
+ * is_utf8_domain: validates UTF-8 domain.
+ */
 extern int
 is_utf8_domain (idn_resconf_t ctx,
                 idn_action_t actions,
@@ -136,12 +192,39 @@ is_utf8_domain (idn_resconf_t ctx,
                 const char *end,
                 bool tld_check);
 
+/*
+ * is_tld: validates if the domain has correct TLD.
+ */
 extern int
 is_tld (const char *start, const char *end);
 
+/*
+ * is_special_domain: validates if the domain is special or reserved.
+ */
 extern int
 is_special_domain (const char *start, const char *end);
 
+/*
+ * is_822_email: check email address as defined in RFC 822.
+ */
+extern int
+is_822_email (const char *email, size_t length, bool tld_check);
+
+/*
+ * is_5321_email: check email address as defined in RFC 5321.
+ */
+extern int
+is_5321_email (const char *email, size_t length, bool tld_check);
+
+/*
+ * is_5322_email: check email address as defined in RFC 5322.
+ */
+extern int
+is_5322_email (const char *email, size_t length, bool tld_check);
+
+/*
+ * is_6531_email: check email address as defined in RFC 6531.
+ */
 extern int
 is_6531_email ( idn_resconf_t ctx,
                 idn_action_t actions,
@@ -149,17 +232,5 @@ is_6531_email ( idn_resconf_t ctx,
                 const char *email,
                 size_t length,
                 bool tld_check);
-
-extern int
-is_822_email (const char *email, size_t length, bool tld_check);
-
-extern int
-is_5321_email (const char *email, size_t length, bool tld_check);
-
-extern int
-is_5322_email (const char *email, size_t length, bool tld_check);
-
-extern int
-is_ipaddr (const char *start, const char *end);
 
 #endif /* EAV_H */
