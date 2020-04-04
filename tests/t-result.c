7e5d5108288c9d2cafd90b196444e0b336d07529
gh0stwizard <vitaliy.tokarev@gmail.com>
2020-04-04 06:28:04 +0300

    return eav_result_t as pointer

--- tests/t-result.c
+++ tests/t-result.c
@@ -81,11 +81,11 @@ main (int argc, char *argv[])
         /* No matter if the test fail because of allow_tld filter,
          * the domain/ip validation are always successful.
          */
-        if (eav.result.is_domain)
+        if (eav.result->is_domain)
             domain++;
-        else if (eav.result.is_ipv4)
+        else if (eav.result->is_ipv4)
             ipv4++;
-        else if (eav.result.is_ipv6)
+        else if (eav.result->is_ipv6)
             ipv6++;
     }
 
